cf7ffb8fbd20ebc257f16f038910dbd790cfb4ce
Deng XiaoLong <Dragonet_d@163.com>
2019-04-21 17:15:08 +0800

    change the source code

---
+++ source-code/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <Universe/>
+  </div>
+</template>
+
+<script>
+import Universe from "./components/universe/Universe";
+export default {
+  name: 'app',
+  components: {
+    Universe
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+  height: 100%;
+}
+html, body {
+  height: 100%;
+  width: 100%;
+}
+
+body {
+  font: normal 1em/1.45em "Helvetica Neue", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  color: #fff;
+  background: radial-gradient(ellipse at bottom, #1C2837 0%, #050608 100%);
+  background-attachment: fixed;
+}
+</style>
